7a78590a632ded6a83445f9f8af08d7f4288b4ce
Gunjan Patel <jalsamn@gmail.com>
2014-12-23 19:26:41 +0000

    Changed SSL to FALSE

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,8 @@ module Lotusstore
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.allow_ssl_in_production = false
+
+
   end
 end
