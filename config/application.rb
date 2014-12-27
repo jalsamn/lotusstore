3c759dae5eddb280f580ff9944000eaff7632744
Gunjan Patel <gpatel@janakfoods.com>
2014-12-27 03:03:29 +0000

    Changed config file

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Lotusstore
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.allow_ssl_in_production = false
+    #config.allow_ssl_in_production = false
 
 
   end
