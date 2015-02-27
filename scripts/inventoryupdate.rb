89a575a6f589f04d9f601f28272a56d9d05154dc
Gunjan Patel <gpatel@janakfoods.com>
2015-02-27 00:04:01 +0000

    s

--- scripts/inventoryupdate.rb
+++ scripts/inventoryupdate.rb
@@ -1,4 +1,4 @@
-result = system("wget --header 'API-AUTHENTICATION: 288c79b9556747d6a66da933720b484a:aecdb2f39e8a4499980dd98a2f083856c547129de799420fa80ed302f51608cd' 'https://lotusmarket.revelup.com/resources/Inventory/?format=json&limit=100' -O 'scripts/jsonoutput/RevInventory.json'")
+result = system("wget --header 'API-AUTHENTICATION: 288c79b9556747d6a66da933720b484a:aecdb2f39e8a4499980dd98a2f083856c547129de799420fa80ed302f51608cd' 'https://lotusmarket.revelup.com/resources/Inventory/?format=json&limit=50000000000' -O 'scripts/jsonoutput/RevInventory.json'")
 if result.nil?
   puts "Error was #{$?}"
 elsif result
