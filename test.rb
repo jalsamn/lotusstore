78f025105aed50bc3e62e205deef43d963787946
Gunjan Patel <gpatel@janakfoods.com>
2015-03-04 22:15:19 +0000

    s

--- test.rb
+++ test.rb
@@ -1,9 +1,10 @@
-@vars = Spree::Variant.all
-     # vendor = Spree::Vendor.find_by_vendorname(row[1])
+@tpois = Spree::TempPo.where(po_id: 47)
+# vendor = Spree::Vendor.find_by_vendorname(row[1])
      # v.vendor_id = vendor.id
      # v.save!
 
-@vars.each do |var|
-var.vendor_id = 5
-  var.save!
+@tpois.each.with_index do |var|
+  puts var.var_id
+  @variant = Spree::Variant.find(var.var_id)
+  puts @variant.name
 end
