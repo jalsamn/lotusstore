29aafcf34990521f3f1668a9f6c5abc1efeb22fb
Gunjan Patel <gpatel@janakfoods.com>
2015-03-07 00:59:20 +0000

    s

--- test.rb
+++ test.rb
@@ -1,10 +1,1 @@
-@tpois = Spree::TempPo.where(po_id: 47)
-# vendor = Spree::Vendor.find_by_vendorname(row[1])
-     # v.vendor_id = vendor.id
-     # v.save!
-
-@tpois.each.with_index do |var|
-  puts var.var_id
-  @variant = Spree::Variant.find(var.var_id)
-  puts @variant.name
-end
+puts "HELLO MR PATEL"
