2dbcef6a585911e4555dc942f8532b2783948add
Gunjan Patel <gpatel@janakfoods.com>
2015-03-21 23:25:57 +0000

    s

--- scripts/productwovendor.rb
+++ scripts/productwovendor.rb
@@ -9,3 +9,16 @@ CSV.open("scripts/jsonoutput/prodwithoutvendor.csv", "wb") do |csv|
   puts var.name
 end
 end
+
+########## BEGIN FTP TRANSFER #########
+
+ftp = Net::FTP.new
+ftp.connect("lotusdelivery.com",21)
+ftp.login("jalsamn","t9zVhn~ckCz4")
+ftp.chdir("/public_html")
+ftp.passive = true
+
+file = "scripts/jsonoutput/prodwithoutvendor.csv"
+
+ftp.putbinaryfile(file, remotefile = File.basename(file))
+ftp.close
